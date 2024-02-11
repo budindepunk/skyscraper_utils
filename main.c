68688edb422a94aef9e3c4dc925d49cf50327b41
Carolina Silva Rode <csilva-r@c4a9c7.42berlin.de>
2024-02-11 17:33:38 +0100

    update main with arg handler grid

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 #include "one_line.c"
 #include "handle_arguments.c"
+#include "handle_arguments_grid.c"
 #include "latin_square.c"
 
 int main(void)
 {
     int i;
     int check;
-
-    // arrays for holding sets of clues
-    int up[4];
-    int down[4];
-    int left[4];
-    int right[4];
     int hold_column[4];
+    int clues_grid[4][4];
 
     char argv[] = "2 3 2 1 2 1 3 2 2 3 1 2 1 2 3 2";
     int	possible_solution[4][4] = {{3, 1, 2, 4}, {1, 3, 4, 2}, {4, 2, 3, 1}, {2, 4, 1, 3}};
     
-    handle_arguments(argv, up, down, left, right);
+    handle_arguments_grid(argv, 4, clues_grid);
+    // handle_arguments(argv, up, down, left, right);
 
     i = 0;
     check = 0;
-    check += check_latin_square(4, possible_solution); //checkinf if the solution has no repeated or >n numbers
+    check += check_latin_square(4, possible_solution); 
+    //checkinf if the solution has no repeated or >n numbers
+    
     while (i < 4)
     {   
         if (check != 0)
         {
             // checking row i and column i against their clues (left-right, up-down respectively)
+            // clues_grid[0] is clues for UP
+            // clues_grid[1] is clues for DOWN
+            // clues_grid[2] is clues for LEFT
+            // clues_grid[3] is clues for RIGHT
             turn_column_into_row(i, 4, possible_solution, hold_column);
-            check += check_row(up[i], down[i], hold_column);
-            check += check_row(left[i], right[i], possible_solution[i]);
+            check += check_row(clues_grid[0][i], clues_grid[1][i], hold_column);
+            check += check_row(clues_grid[2][i], clues_grid[3][i], possible_solution[i]);
         }
         i++;
     }
